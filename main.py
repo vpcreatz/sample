85c3a2927e6413983e3888cb2bc68317a6617642
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-02-18 09:24:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, POR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
-from Script import script 
 from datetime import date, datetime 
 import pytz
 from aiohttp import web

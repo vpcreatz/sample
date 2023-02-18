9fe4daed4181197071ce529fab6b6c40c1b724b2
vpcreatz <119391455+vpcreatz@users.noreply.github.com>
2023-02-18 09:17:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ import re
 import ast
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
-from Script import script
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
@@ -891,4 +890,7 @@ async def manual_filters(client, message, text=False):
                 break
     else:
         return False
+
    
+app = Bot()
+app.run()
